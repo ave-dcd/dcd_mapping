732ba68bf00f33e2dca0ead61c7e4b620fac13bf
Samriddhi <samriddhisingh6079@gmail.com>
2023-06-27 17:26:40 +0530

    Draft for BLAT Alignment

---
+++ mavedb_mapping/blat_alignment.py
@@ -0,0 +1,181 @@
+from Bio import SearchIO
+from gene.query import QueryHandler
+from Bio import SearchIO
+import pandas as pd
+import subprocess
+from class_for_qh import ConcreteClass
+abstract_database_obj = ConcreteClass()
+
+
+def get_gene_data(blat_chr, return_chr, dat):
+    qh = QueryHandler(abstract_database_obj)
+    try:
+        uniprot = dat['uniprot_id']
+        gsymb = qh.normalize(str(f'uniprot:{uniprot}')).gene_descriptor.label
+    except:
+        try:
+            target = dat['target'].split(' ')[0]
+            gsymb = qh.normalize(target).gene_descriptor.label
+        except:
+            return 'NA' # if gsymb cannot be extracted
+   
+    temp = qh.search(gsymb).source_matches
+    source_dict = {}
+    for i in range(len(temp)):
+        source_dict[temp[i].source] = i 
+    
+    if 'HGNC' in source_dict and return_chr == True:
+        chrom = temp[source_dict['HGNC']].records[0].locations[0].chr
+        return chrom
+    
+    if 'Ensembl' in source_dict and return_chr == False and len(temp[source_dict['Ensembl']].records) != 0:
+        for j in range(len(temp[source_dict['Ensembl']].records)):
+            for k in range(len(temp[source_dict['Ensembl']].records[j].locations)):
+                if temp[source_dict['Ensembl']].records[j].locations[k].interval.type == 'SequenceInterval': # Multiple records per source
+                    start = temp[source_dict['Ensembl']].records[j].locations[k].interval.start.value
+                    end = temp[source_dict['Ensembl']].records[j].locations[k].interval.end.value
+                    loc_list = {}
+                    loc_list['start'] = start
+                    loc_list['end'] = end
+                    return loc_list
+    if 'NCBI' in source_dict and return_chr == False and len(temp[source_dict['NCBI']].records) != 0:
+        for j in range(len(temp[source_dict['NCBI']].records)):
+            for k in range(len(temp[source_dict['NCBI']].records[j].locations)):
+                if temp[source_dict['NCBI']].records[j].locations[k].interval.type == 'SequenceInterval':
+                    start = temp[source_dict['NCBI']].records[j].locations[k].interval.start.value
+                    end = temp[source_dict['NCBI']].records[j].locations[k].interval.end.value
+                    loc_list = {}
+                    loc_list['start'] = start
+                    loc_list['end'] = end
+                    return loc_list
+        return 'NA'
+    
+
+
+def extract_blat_output(dat):
+    blat_file = open('blat_query.fa', 'w')
+    blat_file.write('>' + dat['target'] + '\n')
+    blat_file.write(dat['target_sequence'] + '\n')
+    blat_file.close()
+
+    if dat['target_sequence_type'] == 'protein':
+        command = 'blat hg38.2bit -q=prot -t=dnax -minScore=20 blat_query.fa blat_out.psl'
+    else:
+        command = 'blat hg38.2bit -minScore=20 blat_query.fa blat_out.psl'
+        process = subprocess.Popen(command, shell=True)
+    try:
+        output = SearchIO.read('blat_out.psl', 'blat-psl')
+    except:
+        try:
+            command = 'blat hg38.2bit -q=dnax -t=dnax -minScore=20 blat_query.fa blat_out.psl'
+            process = subprocess.Popen(command, shell=True)
+            output = SearchIO.read('blat_out.psl', 'blat-psl')
+        except:
+            return None
+    return output
+
+
+def get_query_and_hit_ranges(output, dat):
+    hit_scores = list()
+    hit_dict = {}
+    use_chr = False
+    chrom = strand = ''
+    coverage = identity = None
+    query_ranges = hit_ranges = list()
+
+    for c in range(len(output)):
+        correct_chr = get_gene_data(output[c].id.strip('chr'), dat=dat, return_chr = True)
+        if correct_chr == output[c].id.strip('chr'):
+            use_chr = True
+            break
+        if correct_chr == 'NA': # Take top scoring hit if target not found using gene normalizer
+            hit_scores = list()
+            for e in range(len(output[c])):
+                hit_scores.append(output[c][e].score)
+            hit_dict[c] = hit_scores
+
+    if use_chr == False:
+        for key in hit_dict:
+            hit_dict[key] = max(hit_dict[key])
+        hit = max(hit_dict, key = hit_dict.get)
+    else:
+        hit = c
+
+    loc_dict = get_gene_data(output[hit].id.strip('chr'), False, dat)
+    
+    hit_starts = list()
+    for n in range(len(output[hit])):
+        hit_starts.append(output[hit][n].hit_start)
+    
+    sub_scores = list()
+    for n in range(len(output[hit])):
+        sub_scores.append(output[hit][n].score)
+    
+    if loc_dict == 'NA':
+        hsp = output[hit][sub_scores.index(max(sub_scores))] # Take top score if no match found 
+    else:
+        hsp = output[hit][hit_starts.index(min(hit_starts, key=lambda x:abs(x - loc_dict['start'])))]
+
+
+    for j in range(len(hsp)):
+        test_file = open('blat_output_test.txt', 'w')
+        test_file.write(str(hsp[j]))
+        test_file.close()
+
+        query_string = ''
+        hit_string = ''
+        strand = hsp[0].query_strand
+        coverage = 100 * (hsp.query_end - hsp.query_start) / output.seq_len
+        coverage = f"{hsp.query_end - hsp.query_start} / {output.seq_len}, {coverage}" 
+        identity = hsp.ident_pct
+
+        test_file = open('blat_output_test.txt', 'r')
+        for k, line in enumerate(test_file):
+            if k == 1:
+                chrom = line.strip('\n')
+            if k == 2:
+                query_string = line.strip('\n')
+            if k == 3:
+                hit_string = line.strip('\n')
+        test_file.close()
+
+        chrom = chrom.split(' ')[9].strip('chr')
+        query_string = query_string.split(' ')
+        hit_string = hit_string.split(' ')
+        query_ranges.append(query_string[2])
+        hit_ranges.append(hit_string[4])   
+
+
+    return chrom, strand, coverage, identity, query_ranges, hit_ranges
+    
+def check_non_human():
+    # TODO
+    pass
+
+
+def mave_to_blat(dat):
+
+    mave_blat_dict = {}
+
+    output = extract_blat_output(dat)
+    if output is not None:
+        chrom, strand, coverage, identity, query_ranges, hit_ranges = get_query_and_hit_ranges(output, dat)
+        qh_dat = {'query_ranges': query_ranges, 'hit_ranges': hit_ranges}
+        qh_dat = pd.DataFrame(data=qh_dat)
+        mave_blat_dict[dat['urn']] = {'chrom': chrom, 'strand': strand, 'target': dat['target'],
+                                  'target_type': dat['target_type'], 'uniprot': dat['uniprot_id'],
+                                  'coverage': coverage, 'identity': identity, 'hits': qh_dat}
+
+    else:
+        qh_dat = {'query_ranges': list('NA'), 'hit_ranges': list('NA')}
+        qh_dat = pd.DataFrame(data=qh_dat)
+        mave_blat_dict[dat['urn']] = {'chrom': 'NA', 'strand': 'NA', 'target': 'NA', 'target_type': 'NA',
+                                          'uniprot': 'NA', 'coverage': 'NA', 'identity': 'NA', 'hits': qh_dat}
+
+
+    print(mave_blat_dict)
+    return mave_blat_dict
+
+
+
+
