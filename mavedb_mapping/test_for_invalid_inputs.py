57cae947ad148f7f6f24fef8dcf0ed96f73727bb
Samriddhi Singh <samriddhisingh6079@gmail.com>
2023-07-13 19:38:06 +0530

    Added tests

---
+++ mavedb_mapping/test_for_invalid_inputs.py
@@ -0,0 +1,19 @@
+from main import main_map
+import pytest
+
+class TestForNonHumanScoresets:
+    def test_for_scoreset_if_blat_successful(self):
+        scoreset = open("urn:mavedb:00000010-a-1","r")
+        with pytest.raises(ValueError):
+            main_map(scoreset)
+
+    def test_for_scoreset_if_blat_not_successful(self):
+        scoreset = open("urn:mavedb:00000004-a-1","r")
+        with pytest.raises(ValueError):
+            main_map(scoreset)
+
+class TestForUnsuccessfulMappingScoresets:
+    def test_for_no_transcripts_found(self):
+        scoreset = open("urn:mavedb:00000097-c-1","r")
+        with pytest.raises(Exception):
+            main_map(scoreset)
