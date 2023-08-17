d5ae9106204d2aeb0409f077d9c284569b68de8b
Samriddhi Singh <samriddhisingh6079@gmail.com>
2023-08-17 22:09:21 +0530

    Truncated sample output, and additional changes in output format

--- mavedb_mapping/output.py
+++ mavedb_mapping/output.py
@@ -4,7 +4,7 @@ import json
 def output(dat, mappings, vrsmaps, blat_dict):
     output_dict = {}
     if mappings:
-        output_dict["sequence_metadata"] = coding(dat, mappings, blat_dict)
+        output_dict["sequence"] = coding(dat, mappings, blat_dict)
         output_dict["mapping"] = mapped_variants(vrsmaps[0])
         return output_dict
     else:
@@ -12,13 +12,14 @@ def output(dat, mappings, vrsmaps, blat_dict):
         pass
 
 
-def coding(dat, mappings, blat_dict):
+def coding(dat, mappings, blat_dict, relation="SO:is_homologous_to"):
     seq_dict = {
-        "Seq": dat["target_sequence"],
+        "target": dat["target_sequence"],
         "RefSeq_prot": mappings["RefSeq_prot"],
         "RefSeq_nuc": mappings["RefSeq_nuc"],
         "gsymb": mappings["gsymb"],
         "chrom": blat_dict["chrom"],
+        "relation": relation
     }
     return seq_dict
 
@@ -27,16 +28,15 @@ def mapped_variants(vrs):
     premapped = vrs["pre_mapping"]
     mapped = vrs["mapped"]
     mapp = list()
-    print(type(mapped))
-    for i in range(len(mapped)):
+    for i in range(3):
         c = maps(premapped[i], mapped[i])
         mapp.append(c)
     return mapp
 
 
-def maps(pre, post, relation="SO:is_homologous_to"):
+def maps(pre, post):
     d = {}
     d["pre_mapped"] = pre
     d["mapped"] = post
-    d["relation"] = relation
+    
     return d
